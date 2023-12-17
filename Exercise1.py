9a7c9a59e641dd01aa69216cf725b64cc985b2f7
opetry16 <everlands.op@gmail.com>
2023-12-17 14:16:21 +0100

    first commit

---
+++ Exercise1.py
@@ -0,0 +1,34 @@
+from queue import Queue
+import time
+
+# Створити чергу заявок
+queue = Queue()
+
+# Функція generate_request():
+def generate_request():
+    # Створити нову заявку (можна використовувати унікальний номер або інші дані)
+    new_request = f"Request {time.time()}"
+    # Додати заявку до черги
+    queue.put(new_request)
+    print(f"New request generated: {new_request}")
+
+# Функція process_request():
+def process_request():
+    # Якщо черга не пуста:
+    if not queue.empty():
+        # Видалити заявку з черги
+        processed_request = queue.get()
+        # Обробити заявку (можна виконати потрібні дії)
+        print(f"Processing request: {processed_request}")
+    else:
+        # Інакше, якщо черга пуста:
+        print("Queue is empty. No requests to process.")
+
+# Головний цикл програми:
+while True:
+    # Виконати generate_request() для створення нових заявок
+    generate_request()
+    # Виконати process_request() для обробки заявок
+    process_request()
+    # Затримка для імітації реального часу між подіями
+    time.sleep(2)
---
+++ Exercise2.py
@@ -0,0 +1,25 @@
+from collections import deque
+
+def is_palindrome(s):
+    # Перетворити рядок у нижній регістр та видалити пробіли
+    s = s.lower().replace(" ", "")
+    
+    # Створити двосторонню чергу та додати символи рядка
+    char_queue = deque(s)
+
+    # Порівняти символи з обох кінців черги
+    while len(char_queue) > 1:
+        if char_queue.popleft() != char_queue.pop():
+            return False
+    
+    # Якщо всі символи співпали, рядок є паліндромом
+    return True
+
+# Приклад використання:
+input_string = "I was on mu Hooney Moon in Nazare and very impressed by GIGANT WAVES "
+result = is_palindrome(input_string)
+
+if result:
+    print(f'Рядок "{input_string}" є паліндромом.')
+else:
+    print(f'Рядок "{input_string}" не є паліндромом.')
